d052fb1a13911fb8ebb5c98ba2ad03b8bddaaf49
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2018-10-23 09:04:15 +0300

    remove customer from basket factory interface

--- src/Basket/Factory/BasketFactoryInterface.php
+++ src/Basket/Factory/BasketFactoryInterface.php
@@ -3,18 +3,15 @@
 namespace PE\Component\ECommerce\Basket\Factory;
 
 use PE\Component\ECommerce\Basket\Model\BasketInterface;
-use PE\Component\ECommerce\Customer\Model\CustomerInterface;
 
 //TODO SessionBasketFactory, PersistentBasketFactory
 //TODO Customer already exists: if logged in - get from db by user, else - create new model
 interface BasketFactoryInterface
 {
     /**
-     * @param CustomerInterface $customer
-     *
      * @return BasketInterface
      */
-    public function loadBasket(CustomerInterface $customer);
+    public function loadBasket();
 
     /**
      * @param BasketInterface $basket
