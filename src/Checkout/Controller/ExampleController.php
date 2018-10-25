1c947d877aa14c81d8ab84013c79f9b534d4567a
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2018-10-25 10:25:09 +0300

    add checkout component for iteract between basket and order

---
+++ src/Checkout/Controller/ExampleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PE\Component\ECommerce\Checkout\Controller;
+
+class ExampleController
+{
+    /**
+     * Process each checkout step
+     */
+    public function actionStep()
+    {}
+
+    /**
+     * Finish checkout process (create order)
+     */
+    public function actionConfirm()
+    {}
+}
